4275e76c5280778163d7962710ea4854d5eaaf85
DevFurina <46137287+Furinaxi@users.noreply.github.com>
2024-08-27 02:00:53 +0200

    Rearranged components for more efficiency
    Also fixed a bug when players had no best runs

--- WoWSpect/Components/GuildInfo.xaml.cs
+++ WoWSpect/Components/GuildInfo.xaml.cs
@@ -1,54 +1,28 @@
 ﻿using System.Collections;
 using System.Windows;
 using System.Windows.Controls;
+using WoWSpect.MVVM.Models.Guilds;
 
 namespace WoWSpect.Components;
 
 public partial class GuildInfo : UserControl
 {
-    public static readonly DependencyProperty GuildNameValueProperty = DependencyProperty.Register(
-        nameof(GuildNameValue), typeof(string), typeof(GuildInfo), new PropertyMetadata(string.Empty));
-
-    public static readonly DependencyProperty GuildFactionValueProperty = DependencyProperty.Register(
-        nameof(GuildFactionValue), typeof(string), typeof(GuildInfo), new PropertyMetadata(string.Empty));
-
-    public static readonly DependencyProperty GuildRealmValueProperty = DependencyProperty.Register(
-        nameof(GuildRealmValue), typeof(string), typeof(GuildInfo), new PropertyMetadata(string.Empty));
-
     public static readonly DependencyProperty GuildMemberListSourceProperty = DependencyProperty.Register(
         nameof(GuildMemberListSource), typeof(IEnumerable), typeof(GuildInfo), new PropertyMetadata(null));
 
-    public static readonly DependencyProperty GuildMemberCountValueProperty = DependencyProperty.Register(
-        nameof(GuildMemberCountValue), typeof(string), typeof(GuildInfo), new PropertyMetadata(string.Empty));
+    public static readonly DependencyProperty GuildMetaDataValueProperty = DependencyProperty.Register(
+        nameof(GuildMetaDataValue), typeof(GuildMetaData), typeof(GuildInfo), new PropertyMetadata(null));
 
-    public string GuildMemberCountValue
+    public GuildMetaData GuildMetaDataValue
     {
-        get { return (string)GetValue(GuildMemberCountValueProperty); }
-        set { SetValue(GuildMemberCountValueProperty, value); }
+        get { return (GuildMetaData)GetValue(GuildMetaDataValueProperty); }
+        set { SetValue(GuildMetaDataValueProperty, value); }
     }
-
+    
     public IEnumerable GuildMemberListSource
     {
         get { return (IEnumerable)GetValue(GuildMemberListSourceProperty); }
         set { SetValue(GuildMemberListSourceProperty, value); }
-    }
-
-    public string GuildRealmValue
-    {
-        get { return (string)GetValue(GuildRealmValueProperty); }
-        set { SetValue(GuildRealmValueProperty, value); }
-    }
-    
-    public string GuildFactionValue
-    {
-        get { return (string)GetValue(GuildFactionValueProperty); }
-        set { SetValue(GuildFactionValueProperty, value); }
-    }
-    
-    public string GuildNameValue
-    {
-        get { return (string)GetValue(GuildNameValueProperty); }
-        set { SetValue(GuildNameValueProperty, value); }
     }
     
     public GuildInfo()
